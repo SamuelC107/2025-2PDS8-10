7b62385786b998c9978194fe347c6c34855be172
Andres <anacleto115@hotmail.com>
2025-08-21 09:20:24 -0500

    Clases base

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+public class Estados
+{
+    public int Id;
+    public string? Nombre;
+}
+
+public class Personas
+{
+    public int Id;
+    public string? Nombre;
+    public decimal Estatura;
+    public bool Vive;
+    public DateTime Fecha;
+    public Estados? Estado;
+}
