d637fd39a3b855dccaed0f980de8da3e47c45d47
Andres <anacleto115@hotmail.com>
2025-08-21 09:58:20 -0500

    Clases final

--- ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e1765fc5e16b68a1f5e6331da4c2af70e26e00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b62385786b998c9978194fe347c6c34855be172")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
