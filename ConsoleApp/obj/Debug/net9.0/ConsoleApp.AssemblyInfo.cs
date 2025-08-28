ea9a44b5a3913bc7456a80bf78183f3b9368182b
SamuelC107 <samuelcorrea1116986@correo.itm.edu.co>
2025-08-27 19:48:29 -0500

    hola

--- ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c892ddcf0d3819f860426d8639824ccf0fdf3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53181e9949e9eb48302d9a256853fc65dadfd4b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
